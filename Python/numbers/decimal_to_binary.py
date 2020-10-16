4c8b669df330053203f522445362da9db003f926
Daniel Barbosa <danielbarbosa.p@outlook.com>
2020-10-16 09:53:44 -0300

    Creating the function to_binary

--- Python/numbers/decimal_to_binary.py
+++ Python/numbers/decimal_to_binary.py
@@ -1,23 +1,30 @@
+# It converts decimal to binary
+
 decimal_number = int(input("Enter the decimal number: "))
-operator_number = decimal_number
-binary = 0
-binary_inverted = []
 
-while(operator_number >= 1):
-    if(operator_number == 1):
-        binary_inverted.append(operator_number)
-        break
-    else:
-        binary = operator_number%2
-        binary_inverted.append(binary)
-        operator_number = operator_number//2
+def toBinary(decimal_number):
 
-counter = len(binary_inverted) - 1
-binary_list = []
+    operator_number = decimal_number
+    binary = 0
+    binary_inverted = []
 
-while(counter >= 0):
-    binary_list.append(binary_inverted[counter])
-    counter -=1
+    while(operator_number >= 1):
+        if(operator_number == 1):
+            binary_inverted.append(operator_number)
+            break
+        else:
+            binary = operator_number%2
+            binary_inverted.append(binary)
+            operator_number = operator_number//2
+
+    counter = len(binary_inverted) - 1
+    binary_string = ""
+
+    while(counter >= 0):
+        binary_string += str(binary_inverted[counter])
+        counter -=1
     
+    return binary_string
 
-print(binary_list)
+
+print(toBinary(decimal_number)) #Show the binary result as a string
